0a00624f43bbbc313ae13481f9595c19ddaf46ba
mac <shirohebiui@gmail.com>
2023-11-14 09:25:29 +0900

    dp2

--- #DP/G/17404(G4)/17404 solve.c
+++ #DP/G/17404(G4)/17404 solve.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #define min(a, b) (a<b?a:b)
 #define INF 1000
-void test();
 void input();
 
-int house[1000];
 int RGB[1000][3];
 int DP[1000][3];
 int N;
@@ -12,11 +10,11 @@ int N;
 void dpfunc() { 
     for(int i=1; i<N; i++) {
         DP[i][0] = RGB[i][0] + min(DP[i-1][1], DP[i-1][2]); 
-        //R consider prev GB
+        //R consider prev G,B
         DP[i][1] = RGB[i][1] + min(DP[i-1][0], DP[i-1][2]); 
-        //G consider prev RB
+        //G consider prev R,B
         DP[i][2] = RGB[i][2] + min(DP[i-1][0], DP[i-1][1]); 
-        //B consider prev RG
+        //B consider prev R,G
     }
 }
 
@@ -27,7 +25,6 @@ int find_best() {
         DP[0][(select + 1) % 3] = INF;
         DP[0][(select + 2) % 3] = INF;
         dpfunc();
-        //test();
         best = min(best, min(DP[N-1][(select + 1) % 3], DP[N-1][(select + 2) % 3]));
     }
     return best;
@@ -46,19 +43,4 @@ void input() {
             scanf(" %d", &RGB[i][color]);
         }
     }
-}
-
-void test() {
-    char ch[4] = "RGB";
-    printf("-----test-----\n");
-    for(int color=0; color<3; color++)
-        printf("%2c  ", ch[color]);
-    printf("\n");
-    for(int i=0; i<N; i++) {
-        for(int color=0; color<3; color++) {
-            printf("%3d ", DP[i][color]);
-        }
-        printf("\n");
-    }
-    printf("\n");
 }
