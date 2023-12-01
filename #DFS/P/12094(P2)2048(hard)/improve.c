3ad185ed981e627972520517ab6c5e4b6aaf18b7
mac <shirohebiui@gmail.com>
2023-12-01 14:49:13 +0900

    2048(hard) improve

--- #DFS/P/12094(P2)2048(hard)/improve.c
+++ #DFS/P/12094(P2)2048(hard)/improve.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #define max(a,b) (a>b?a:b)
-#define UP 0
-#define DOWN 1
-#define LEFT 2
-#define RIGHT 3
+#define UP   -1
+#define DOWN  1
+#define LEFT -2
+#define RIGHT 2
 #define FIND_LV 10
-typedef unsigned int u16;
-size_t u16_size = sizeof(u16);
+typedef unsigned char u_char;
+size_t u_char_size = sizeof(u_char);
 size_t mem_clear_size; //board clear size (func move)
 size_t mem_line_size;  //cmp line size (func IsNotChange)
 int i, j;
 
 void input_board();
-u16 board[FIND_LV+1][20][20];
+u_char board[FIND_LV+1][20][20];
 //lv0=root lv1 lv2 lv3 .... board
 int size;
 
 void dfs(int direct, int lv);
-int max_tmp; //dfs에서의 단계에 따른 max값
-int final_max; //최종 max값
+u_char max_tmp; //dfs에서의 단계에 따른 max값
+u_char final_max; //최종 max값
 
 void move(int direct, int lv); //현재 단계에서 다음 단계로 넘기기
 void move_up(int lv);
@@ -29,6 +29,7 @@ void move_right(int lv);
 int cnt, next; //move함수에서 쓰는 변수들
 
 int IsNotUsefull(int lv);
+u_char possible_mxbit;
 int IsNotChange(int lv);
 
 int main() {
@@ -43,8 +44,8 @@ int main() {
 
 void input_board() {
     scanf(" %d", &size);
-    mem_clear_size = size*u16_size*20;
-    mem_line_size  = size*u16_size;
+    mem_clear_size = size*u_char_size*20;
+    mem_line_size  = size*u_char_size;
     int in, exp;
     for(i=0; i<size; i++) {
         for(j=0; j<size; j++) {
@@ -108,7 +109,6 @@ void move_up(int lv) {
                     board[next][cnt/2][i] = board[lv][j][i];
                 } else if(board[next][cnt/2][i] == board[lv][j][i]) {
                     //같은수
-                    // board[next][cnt/2][i] += board[lv][j][i];
                     board[next][cnt/2][i]++;
                 } else {
                     //다른수
@@ -131,7 +131,6 @@ void move_down(int lv) {
                     board[next][cnt/2][i] = board[lv][j][i];
                 } else if(board[next][cnt/2][i] == board[lv][j][i]) {
                     //같은수
-                    // board[next][cnt/2][i] += board[lv][j][i];
                     board[next][cnt/2][i]++;
                 } else {
                     //다른수
@@ -154,7 +153,6 @@ void move_left(int lv) {
                     board[next][i][cnt/2] = board[lv][i][j];
                 } else if(board[next][i][cnt/2] == board[lv][i][j]) {
                     //같은수
-                    // board[next][i][cnt/2] += board[lv][i][j];
                     board[next][i][cnt/2]++;
                 } else {
                     //다른수
@@ -177,7 +175,6 @@ void move_right(int lv) {
                     board[next][i][cnt/2] = board[lv][i][j];
                 } else if(board[next][i][cnt/2] == board[lv][i][j]) {
                     //같은수
-                    // board[next][i][cnt/2] += board[lv][i][j];
                     board[next][i][cnt/2]++;
                 } else {
                     //다른수
@@ -194,8 +191,7 @@ void move_right(int lv) {
 int IsNotUsefull(int lv) {
     if(lv == 0 ) return 0;
     //현재 2^6, 앞으로 3번더 움직일 수 있다면 2^9가 최대기대치 
-    // int possible_mxbit = max_tmp<<(FIND_LV - lv);
-    u16 possible_mxbit = max_tmp + (FIND_LV - lv);
+    possible_mxbit = max_tmp + (FIND_LV - lv);
     if(possible_mxbit <= final_max) return 1;
     return 0;
 }
@@ -206,4 +202,3 @@ int IsNotChange(int lv) {
         if(memcmp(&board[lv][i][0], &board[lv-1][i][0], mem_line_size)) return 0;
     return 1;
 }
-
